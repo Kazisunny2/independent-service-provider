52184c3890fff81a992b6909393239d9652325be
Kazisunny2 <saidul.bit@gmail.com>
2022-04-17 04:44:36 +0200

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Services from '../Services/Services';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 const Home = () => {
-    return (
-        <div>
-            <Services></Services>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Services></Services>
+    </div>
+  );
 };
 
 export default Home;
